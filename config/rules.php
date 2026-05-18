78f45e31aad587cc45906910a2abc0ff2bd55bec
Anton Vix <Gordan.ei@protonmail.ch>
2026-05-18 20:14:12 +0400

    Add rules configuration file for Rector with defined rules

---
+++ config/rules.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Vix\RectorRules\AddTypedClassConstantRector;
+use Vix\RectorRules\CollapseSequentialStrReplaceRector;
+use Vix\RectorRules\ExtractAssignmentFromIfConditionRector;
+use Vix\RectorRules\NullableBoolReturnToFalseRector;
+use Vix\RectorRules\ReplaceMultipleEqualWithInArrayRector;
+use Vix\RectorRules\Yii2FindAllIdShortcutRector;
+use Vix\RectorRules\Yii2FindOneFindAllShortcutRector;
+use Vix\RectorRules\Yii2FindOneIdShortcutRector;
+use Vix\RectorRules\Yii2PropertyAccessRector;
+use Vix\RectorRules\Yii2UseExistsInsteadOfCountRector;
+use Vix\RectorRules\Yii2UseExistsInsteadOfOneNotNullRector;
+use Vix\RectorRules\Yii2UserFindOneToIdentityRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rules([
+        AddTypedClassConstantRector::class,
+        CollapseSequentialStrReplaceRector::class,
+        ExtractAssignmentFromIfConditionRector::class,
+        NullableBoolReturnToFalseRector::class,
+        ReplaceMultipleEqualWithInArrayRector::class,
+        Yii2FindAllIdShortcutRector::class,
+        Yii2FindOneFindAllShortcutRector::class,
+        Yii2FindOneIdShortcutRector::class,
+        Yii2PropertyAccessRector::class,
+        Yii2UseExistsInsteadOfCountRector::class,
+        Yii2UseExistsInsteadOfOneNotNullRector::class,
+        Yii2UserFindOneToIdentityRector::class,
+    ]);
+};
